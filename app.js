fcdf04fcd0cb908a7664b281cbb10c1804f0317d
arman858 <36173768+arman858@users.noreply.github.com>
2018-03-11 17:50:57 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+const calculateTerm = term => {
+  let numbers = [];
+  term.split("*").forEach(elm => {
+    numbers = [ ...numbers, ...elm.split("/")]
+  });
+
+  let result = 1;
+  numbers.forEach(currentNumber => {
+    const splittedNumber = term.split(currentNumber)[0];
+    const sign = splittedNumber[splittedNumber.length - 1];
+    if (sign === '/') {
+      result /= parseInt(currentNumber, 10);
+    } else {
+      result *= parseInt(currentNumber, 10);
+    }
+  })
+
+  return result;
+}
+
+const calculateResult = expression => {
+  let terms = [];
+  expression.split("+").forEach(elm => {
+    terms = [ ...terms, ...elm.split("-")]
+  });
+
+  let result = 0;
+  terms.forEach(currentTerm => {
+    const splittedTerm = expression.split(currentTerm)[0];
+    const sign = splittedTerm[splittedTerm.length - 1];
+    if (sign === '-') {
+      result -= calculateTerm(currentTerm);
+    } else {
+      result += calculateTerm(currentTerm);
+    }
+  })
+
+  return result;
+}
+
+const textField = document.getElementById("expression-field");
+const buttons =
+  document.querySelectorAll('.button-group button');
+const equalsButton = document.getElementById("equals");
+const clearButton = document.getElementById("clear");
+
+clearButton.addEventListener('click', event => {
+  textField.value = '0';
+})
+
+buttons.forEach(currentButton => {
+  if (!currentButton.id) {
+    currentButton.addEventListener('click', event => {
+      if (textField.value === '0') {
+        textField.value = currentButton.innerText;
+      } else {
+        textField.value += currentButton.innerText;
+      }
+    })
+  }
+})
+
+equalsButton.addEventListener('click', event => {
+  textField.value = calculateResult(textField.value);
+})
